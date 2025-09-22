2f333fbc0228fcf960094f2126ef9fd3c05f7d03
Ann Bondar <ganna.bondar@stud.onu.edu.ua>
2025-09-22 19:26:30 -0400

    /

---
+++ Third_Console_App/Interface+classes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// generic інтерфейс з generic методом
+public interface IGenericInterface<T>
+{
+    T Process(T input);
+}
+
+// звичайний інтерфейс з одним методом
+public interface ISimpleInterface
+{
+    void ShowMessage(string message);
+}
+
+// абстрактний клас з абстрактним і звичайним методами
+public abstract class AbstractBase
+{
+    public abstract void AbstractMethod();
+
+    public void NormalMethod()
+    {
+        Console.WriteLine("This is a normal method in AbstractBase");
+    }
+}
+
+// клас, який наслідує абстрактний клас і реалізує обидва інтерфейси
+public class ConcreteClass : AbstractBase, IGenericInterface<int>, ISimpleInterface
+{
+    public override void AbstractMethod()
+    {
+        Console.WriteLine("Implementation of abstract method");
+    }
+
+    public int Process(int input)
+    {
+        return input * 2;
+    }
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine("Message: " + message);
+    }
+}
---
+++ Third_Console_App/Methods with tasks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class TaskExamples
+{
+    // метод, що запускає 3 таски з випадковою затримкою та очікує завершення
+    public static async Task RunTasksAndWait()
+    {
+        Random rnd = new Random();
+
+        Task[] tasks = new Task[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int taskNum = i + 1;
+            tasks[i] = Task.Run(async () =>
+            {
+                int delay = rnd.Next(1000, 3001);
+                await Task.Delay(delay);
+                Console.WriteLine($"Task {taskNum} finished after {delay} ms");
+            });
+        }
+
+        await Task.WhenAll(tasks);
+        Console.WriteLine("All tasks finished!");
+    }
+
+    // метод, що запускає 3 таски і пише результат першої завершеної
+    public static async Task RunTasksAndPrintFirstResult()
+    {
+        Random rnd = new Random();
+
+        Task<string>[] tasks = new Task<string>[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int taskNum = i + 1;
+            tasks[i] = Task.Run(async () =>
+            {
+                int delay = rnd.Next(1000, 3001);
+                await Task.Delay(delay);
+                return $"Task {taskNum} result (after {delay} ms)";
+            });
+        }
+
+        Task<string> firstFinished = await Task.WhenAny(tasks);
+        Console.WriteLine("First completed: " + await firstFinished);
+    }
+}
